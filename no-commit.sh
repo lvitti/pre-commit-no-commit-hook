c12d4249bd2628bfa865c4feccce702622f81582
Luciano Vitti <luciano@vitti.com.ar>
2022-06-03 13:15:13 -0300

    remove --cached

--- no-commit.sh
+++ no-commit.sh
@@ -2,11 +2,11 @@
 
 # https://stackoverflow.com/a/20574486/3041568
 
-noCommitCount=$(git diff --no-ext-diff --cached | egrep -i --count "#no-commit#")
+noCommitCount=$(git diff --no-ext-diff | egrep -i --count "#no-commit#")
 if [ "$noCommitCount" -ne "0" ]; then
-   echo "WARNING: You are attempting to commit changes which include a 'no-commit'."
+   echo "WARNING: You are attempting to commit changes which include a '#no-commit#'."
    echo "Please check the following files:"
-   git diff --no-ext-diff --cached --name-only -i -G"#no-commit#" | sed 's/^/   - /'
+   git diff --no-ext-diff --name-only -i -G"#no-commit#" | sed 's/^/   - /'
    echo
    echo "You can ignore this warning by running the commit command with '--no-verify'"
    exit 1
